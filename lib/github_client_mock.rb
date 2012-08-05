50b9471a2c6ade463fc3d951c1f1f6621061dfeb
Dinduks <samy@dindane.com>
2012-08-05 12:10:33 +0200

    Refactor the GitHubClientMock::call method

--- lib/github_client_mock.rb
+++ lib/github_client_mock.rb
@@ -1,15 +1,8 @@
 class GitHubClientMock
   def call(path)
-    case path
-    when '/users/dinduks/repos'
-      resp = File.open(File.dirname(__FILE__) + '/../resources/users_dinduks_repos.json').read
-    when '/users/auserthatdoesntexist/repos'
-      resp = File.open(File.dirname(__FILE__) + '/../resources/users_auserthatdoesntexist_repos.json').read
-    when '/legacy/repos/search/dinduks'
-      resp = File.open(File.dirname(__FILE__) + '/../resources/legacy_repos_search_dinduks.json').read
-    when '/legacy/repos/search/arepothatdoesntexist'
-      resp = File.open(File.dirname(__FILE__) + '/../resources/legacy_repos_search_arepothatdoesntexist.json').read
-    else
+    begin
+      resp = File.open(File.dirname(__FILE__) + "/../resources/#{path.gsub('/', '_')[1, path.length]}.json").read
+    rescue
       resp = "{}"
     end
 
