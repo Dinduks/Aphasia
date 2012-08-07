88ced21fff31d2cb0dcd83ba4bd66cdcc74c986f
Dinduks <samy@dindane.com>
2012-08-07 20:48:41 +0200

    Use the converters

--- lib/aphasia.rb
+++ lib/aphasia.rb
@@ -6,29 +6,27 @@ class Aphasia
   def find_repos(keyword)
     resp = @http_client.call("/legacy/repos/search/#{keyword}")
 
-    repositories_array = Array.new
-    resp['repositories'].each do |repository_hash|
-      repo = RepositoryConverter.fill_object_from_legacy_hash repository_hash
-      repositories_array.push(repo)
+    repos = create_repos_array(resp['repositories']) do |hash|
+      RepositoryConverter.fill_object_from_legacy_hash hash
     end
-    repositories_array
   end
 
   def find_user_repo(username)
     resp = @http_client.call("/users/#{username}/repos")
     raise UserNotFound.new(username) if resp['message'].to_s == 'Not Found' if resp.is_a? Hash
-    create_repos_array(resp)
+
+    create_repos_array(resp) do |hash|
+      Repository.new.fill_from_hash! hash
+    end
   end
 
   private
   def create_repos_array(repositories)
     repositories_array = Array.new
-
     repositories.each do |repository_hash|
-      repo = Repository.new.fill_from_hash!(repository_hash)
+      repo = yield repository_hash
       repositories_array.push(repo)
     end
-
     repositories_array
   end
 end
