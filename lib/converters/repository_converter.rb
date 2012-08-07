4a7ae2bc9b31d0c1f4aaa1743c7d1c2f083aecf6
Dinduks <samy@dindane.com>
2012-08-07 20:33:02 +0200

    Add the RepositoryConverter::fill_object_from_hash converter

--- lib/converters/repository_converter.rb
+++ lib/converters/repository_converter.rb
@@ -21,4 +21,36 @@ class RepositoryConverter
 
     repo
   end
+
+  def self.fill_object_from_hash(hash)
+    repo = Repository.new
+
+    repo.html_url = hash['html_url']
+    repo.url    = hash['url']
+    repo.id    = hash['id']
+
+    owner             = User.new
+    owner.login       = hash['owner']['login']
+    owner.gravatar_id = hash['owner']['gravatar_id']
+    owner.url         = hash['owner']['url']
+    owner.avatar_url  = hash['owner']['avatar_url']
+    owner.id          = hash['owner']['id']
+    repo.owner        = owner
+
+    repo.name        = hash['name']
+    repo.full_name   = hash['full_name']
+    repo.description = hash['description']
+    repo.homepage    = hash['homepage']
+    repo.languages   = Array.new.push hash['language']
+    repo.private     = hash['private'].to_s == 'false' ? false : true
+    repo.fork        = hash['fork'].to_s    == 'false' ? false : true
+    repo.forks       = hash['forks']
+    repo.watchers    = hash['watchers']
+    repo.size        = hash['size']
+    repo.pushed_at   = DateTime.iso8601 hash['pushed_at']
+    repo.created_at  = DateTime.iso8601 hash['created_at']
+    repo.updated_at  = DateTime.iso8601 hash['created_at']
+
+    repo
+  end
 end
--- spec/converters/repository_converter_spec.rb
+++ spec/converters/repository_converter_spec.rb
@@ -23,12 +23,53 @@ describe 'RepositoryConverter' do
       @repository.homepage.should       == 'http://www.dinduks.com/'
       @repository.languages.should be_an Array
       @repository.languages.should include 'JavaScript'
-      @repository.private.should        == false
-      @repository.fork.should           == false
+      @repository.private.should be_false
+      @repository.fork.should    be_false
       @repository.watchers.should       == 2
       @repository.size.should           == 5192
       @repository.pushed_at.should be_a  DateTime
       @repository.created_at.should be_a DateTime
+    end
+  end
+
+  describe 'fill_object_from_hash' do
+    before do
+      http_client = GitHubClientMock.new
+      hash = http_client.call '/users_dinduks_repos'
+      @repository = RepositoryConverter.fill_object_from_hash hash[0]
+    end
+
+    it 'should return a Repository object' do
+      @repository.should be_a Repository
+    end
+
+    it 'should correctly fill in the objects' do
+      @repository.html_url.should == 'https://github.com/Dinduks/furry-octo-ninja'
+      @repository.url.should      == 'https://api.github.com/repos/Dinduks/furry-octo-ninja'
+      @repository.id.should       == 4118257
+
+      @repository.owner.should be_a User
+      @repository.owner.login.should       == 'Dinduks'
+      @repository.owner.gravatar_id.should == 'd774db9cf2913d59b6e0e95662522b20'
+      @repository.owner.url.should         == 'https://api.github.com/users/Dinduks'
+      @repository.owner.avatar_url.should  == 'https://secure.gravatar.com/avatar/d774db9cf2913d59b6e0e95662522b20?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png'
+      @repository.owner.id.should          == 504977
+
+      @repository.name.should        == 'furry-octo-ninja'
+      @repository.full_name.should   == 'Dinduks/furry-octo-ninja'
+      @repository.description.should == 'This is the application I use to share my code snippets'
+      @repository.homepage.should    == 'http://snippets.dinduks.com'
+      @repository.languages.should be_an Array
+      @repository.languages.should include 'Ruby'
+
+      @repository.private.should be_false
+      @repository.fork.should    be_false
+      @repository.forks.should    == 1
+      @repository.watchers.should == 1
+      @repository.size.should     == 172
+      @repository.pushed_at.should  be_a DateTime
+      @repository.created_at.should be_a DateTime
+      @repository.updated_at.should be_a DateTime
     end
   end
 end
