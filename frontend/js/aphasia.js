950312b89f0461aada1b2979d3975504cda5c99f
Dinduks <samy@dindane.com>
2012-08-10 22:10:19 +0200

    WIP - Working on the UI

--- frontend/js/aphasia.js
+++ frontend/js/aphasia.js
@@ -1,10 +1,11 @@
 var module = angular.module('aphasia', ['ngResource']);
 
+// This object will handle the Ajax API calls
 module.factory('Repository', function($resource) {
   return $resource('http://localhost::port/repos/:keyword', {keyword: '', port: 4567}, {
     query: {method: 'GET', isArray: true},
   });
-})
+});
 
 function AphasiaCtrl($scope, Repository) {
   $scope.updateRepositoriesList = function() {
@@ -12,16 +13,30 @@ function AphasiaCtrl($scope, Repository) {
     $('.main-panel').fadeOut('slow');
 
     Repository.query({keyword: $scope.repositoryName}, function (repositories) {
+      if (repositories.length == 0) {
+        repositories.push({ full_name: 'No repository called "' + $scope.repositoryName + '" were found' });
+      }
       $scope.repositories = repositories;
 
       loadingAnimation('hide');
-
-      if ($('.main-panel').css('display') == 'none')
-        $('.main-panel').fadeIn('slow');
-
+      $scope.showSearchResults();
       setTimeout(function() {$('.repositories-list a').first().focus();}, 2500);
     });
   }
+
+  $scope.showRepositoryInfo = function(fullName) {
+    hideSearchResults();
+    $scope.parseRepositoryInfo();
+    displayRepositoryInfo();
+  }
+
+  $scope.showSearchResults = function() {
+    $('.search-results').fadeIn();
+    $('.repository-info').slideUp();
+    $('.repositories-list a').first().focus();
+  }
+
+  $scope.parseRepositoryInfo = function() {}
 }
 
 function loadingAnimation(action) {
@@ -33,3 +48,11 @@ function loadingAnimation(action) {
     throw 'loadingAnimation() has no "' + action + '" option.';
 }
 
+function hideSearchResults() {
+  $('.search-results').slideUp();
+  $('.search-results').css('display', 'block');
+}
+
+function displayRepositoryInfo() {
+  $('.repository-info').slideDown();
+}
