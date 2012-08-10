a3af558b1e3642743579bbcbd9a232edd576bf37
Dinduks <samy@dindane.com>
2012-08-10 13:02:15 +0200

    Add few UI features

--- frontend/js/aphasia.js
+++ frontend/js/aphasia.js
@@ -8,12 +8,28 @@ module.factory('Repository', function($resource) {
 
 function AphasiaCtrl($scope, Repository) {
   $scope.updateRepositoriesList = function() {
-    if ((repositoryName = $scope.repositoryName).length < 3)
-      return;
+    loadingAnimation('show');
+    $('.main-panel').fadeOut('slow');
 
-    Repository.query({keyword: repositoryName}, function (repositories) {
+    Repository.query({keyword: $scope.repositoryName}, function (repositories) {
       $scope.repositories = repositories;
+
+      loadingAnimation('hide');
+
+      if ($('.main-panel').css('display') == 'none')
+        $('.main-panel').fadeIn('slow');
+
+      setTimeout(function() {$('.repositories-list a').first().focus();}, 2500);
     });
   }
+}
+
+function loadingAnimation(action) {
+  if (action == 'hide')
+    $('.repository-input').css('background-image', 'none');
+  else if (action == 'show')
+    $('.repository-input').css('background-image', 'url("./img/loading.gif")');
+  else
+    throw 'loadingAnimation() has no "' + action + '" option.';
 }
 
