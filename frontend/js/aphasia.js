a8fc492a767cf0152a4c7465b6155d0575b880c3
Dinduks <samy@dindane.com>
2012-08-11 20:40:31 +0200

    Add contributors list display

--- frontend/js/aphasia.js
+++ frontend/js/aphasia.js
@@ -1,18 +1,27 @@
+"use strict";
+
 var module = angular.module('aphasia', ['ngResource']);
 
-// This object will handle the Ajax API calls
+// This object will handle the Ajax API calls concerning the repos
 module.factory('Repository', function($resource) {
   return $resource('http://localhost::port/repos/:keyword', {keyword: '', port: 4567}, {
     query: {method: 'GET', isArray: true},
   });
 });
 
-function AphasiaCtrl($scope, Repository) {
+// This one will handle the calls related to the commits
+module.factory('Commit', function($resource) {
+  return $resource('http://localhost::port/repo/:username/:repo/commits', {repository_full_name: '', port: 4567}, {
+    query: {method: 'GET', isArray: true},
+  });
+});
+
+function AphasiaCtrl($scope, Repository, Commit) {
   $scope.updateRepositoriesList = function() {
     loadingAnimation('show');
     $('.main-panel').fadeOut('slow');
 
-    Repository.query({keyword: $scope.repositoryName}, function (repositories) {
+    Repository.query({keyword: $scope.repositoryName}, function(repositories) {
       if (repositories.length == 0) {
         repositories.push({ full_name: 'No repository called "' + $scope.repositoryName + '" were found' });
       }
@@ -24,9 +33,16 @@ function AphasiaCtrl($scope, Repository) {
     });
   }
 
-  $scope.showRepositoryInfo = function(fullName) {
+  $scope.showRepositoryInfo = function(repositoryFullName) {
     hideSearchResults();
-    $scope.parseRepositoryInfo();
+
+    var username = repositoryFullName.split('/')[0]
+    var repo     = repositoryFullName.split('/')[1]
+    Commit.query({username: username, repo: repo}, function(commits) {
+      $scope.parseRepositoryInfo(commits);
+      $scope.totalCommits = commits.length;
+    });
+
     displayRepositoryInfo();
   }
 
@@ -36,7 +52,10 @@ function AphasiaCtrl($scope, Repository) {
     $('.repositories-list a').first().focus();
   }
 
-  $scope.parseRepositoryInfo = function() {}
+  $scope.parseRepositoryInfo = function(commits) {
+    var contributors = getContributors(commits);
+    $scope.contributors = contributors;
+  }
 }
 
 function loadingAnimation(action) {
@@ -56,3 +75,23 @@ function hideSearchResults() {
 function displayRepositoryInfo() {
   $('.repository-info').slideDown();
 }
+
+function getContributors(commits) {
+  var contributors = {};
+
+  for (var i=0; i < commits.length; i++) {
+    var author = commits[i].author;
+    if (!contributors[author.login]) {
+      contributors[author.login] = {
+        contributionsCounter: 1,
+        avatar_url: author.avatar_url,
+        url: author.url,
+      }
+    } else {
+      contributors[author.login].contributionsCounter++;
+    }
+  }
+
+  return contributors;
+}
+
